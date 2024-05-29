d76d1a953cd9dad74c96648adb650687e96b6cc1
Balashov Igor <igor2233balashov@gmail.com>
2024-05-29 10:21:48 +0300

    Laptop commit

--- locale/compile_translations.py
+++ locale/compile_translations.py
@@ -2,8 +2,10 @@ import polib
 
 locales = ['en', 'ru']
 
-for locale in locales:
-    po_path = f'{locale}/LC_MESSAGES/messages.po'
-    mo_path = f'{locale}/LC_MESSAGES/messages.mo'
-    po = polib.pofile(po_path)
-    po.save_as_mofile(mo_path)
+if __name__ == "__main__":
+    for locale in locales:
+        po_path = f'{locale}/LC_MESSAGES/messages.po'
+        mo_path = f'{locale}/LC_MESSAGES/messages.mo'
+        po = polib.pofile(po_path)
+        po.save_as_mofile(mo_path)
+    print("Translation files compiled successfully!")
