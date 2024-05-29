a2d8481135e3186b39435bc097862854029b4ddc
Balashov Igor <igor2233balashov@gmail.com>
2024-05-29 09:46:02 +0300

    Laptop commit

--- locale/compile_translations.py
+++ locale/compile_translations.py
@@ -3,7 +3,7 @@ import polib
 locales = ['en', 'ru']
 
 for locale in locales:
-    po_path = f'locale/{locale}/LC_MESSAGES/messages.po'
-    mo_path = f'locale/{locale}/LC_MESSAGES/messages.mo'
+    po_path = f'{locale}/LC_MESSAGES/messages.po'
+    mo_path = f'{locale}/LC_MESSAGES/messages.mo'
     po = polib.pofile(po_path)
     po.save_as_mofile(mo_path)
